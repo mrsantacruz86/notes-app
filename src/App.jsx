d11eab13a30cce6a045bbfc5c317a5cf21d1ccd0
Nelson Diaz <abnerdc86@gmail.com>
2025-08-08 18:27:06 -0400

    Added Delete note button

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,20 @@ import NoteForm from './components/NoteForm';
 import NoteList from './components/NoteList';
 const App = () => {
   const [notes, setNotes] = useState([]);
+  const deleteNote = (id) => {
+    const confirmDelete = window.confirm(
+      'Are you sure you want to delete the note?'
+    );
+    if (confirmDelete) {
+      setNotes(notes.filter((note) => note.id !== id));
+    }
+  };
+
   return (
     <div className="max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg">
       <h2 className="text font-bold mb-4 text-center">📓Notes App</h2>
       <NoteForm notes={notes} setNotes={setNotes} />
-      <NoteList notes={notes} />
+      <NoteList notes={notes} deleteNote={deleteNote} />
     </div>
   );
 };
