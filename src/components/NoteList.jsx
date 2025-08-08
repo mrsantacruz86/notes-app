57dfbf368f64550b1587ee1c53324bd42f7610c1
Nelson Diaz <abnerdc86@gmail.com>
2025-08-08 00:05:40 -0400

    Fix rounded corners in the note list items.

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -7,7 +7,7 @@ const NoteList = ({ notes }) => {
       {notes.map((note) => (
         <div
           key={note.id}
-          className="p-4 bg-white rouded-lg shadow-md border-l-4"
+          className="p-4 bg-white rounded-lg shadow-md border-l-4"
         >
           <h3 className="text-lg font-bold">{note.title}</h3>
           <p className="tex-sm text-gray-600">
@@ -18,6 +18,7 @@ const NoteList = ({ notes }) => {
             <strong>Priority:</strong>
             {note.priority}
           </p>
+          <p className="mt-2">{note.description} </p>
         </div>
       ))}
     </div>
